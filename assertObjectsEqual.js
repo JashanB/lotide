fde2f5f628026aec6332614a79b2d0ab64360514
Jashan <jashan_brar50@hotmail.com>
2019-11-27 23:46:10 +0000

    added and tested assert obj equal

---
+++ assertObjectsEqual.js
@@ -0,0 +1,35 @@
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  for (key of Object.keys(object1)) {
+    if (Array.isArray(object1[key])) {
+      if (eqArrays(object1[key], object2[key]) === false) {
+        return false;
+      }
+    } else {
+      if (object1[key] !== object2[key]) { 
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertation Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑Assertation Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+const obj1 = {
+  name: 'brad',
+  age: 25
+}
+const obj2 = {
+  name: 'chad',
+  age: 25
+}
+assertObjectsEqual(obj1, obj2)
