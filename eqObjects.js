1f07ae1f54e11c30429d54bf2c0cfb2c58967770
Jashan <jashan_brar50@hotmail.com>
2019-12-04 17:58:27 +0000

    made recursive eq objects/arrays

--- eqObjects.js
+++ eqObjects.js
@@ -1,6 +1,6 @@
 const assertEqual = require('./assertEqual');
 const eqArrays = require('./eqArrays');
-const eqObjects = function (object1, object2) {
+const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
@@ -9,8 +9,8 @@ const eqObjects = function (object1, object2) {
       if (eqArrays(object1[key], object2[key]) === false) {
         return false;
       }
-    // } else if (typeof object1[key] === 'object') {
-    //   eqArrays(object1[key], object2[key]);
+    } else if (typeof object1[key] === 'object') {
+      eqObjects(object1[key], object2[key]);
     } else {
       if (object1[key] !== object2[key]) {
         return false;
@@ -35,7 +35,7 @@ console.log(eqObjects(cd, dc) === true); // => true
 const cd2 = { c: "1", d: ["2", 3, 4] };
 console.log(eqObjects(cd, cd2) === false); 
 
-// const cd3 = { c: "1", d: { e: "2"} };
-// const dc3 = { c: "1", d: { e: "3"} };
+const cd3 = { c: "1", d: { e: "2"} };
+const dc3 = { c: "1", d: { e: "2"} };
 // // const dc3 = { d: { e: "2", f: 3}, c: "1" };
-// console.log(eqObjects(cd3, dc3) === true); // => true
+console.log(eqObjects(cd3, dc3) === true); // => true
